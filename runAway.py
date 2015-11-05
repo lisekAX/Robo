453af04c22d7b29bdf28bc4d21fa084e0f1c84c2
lisekAX <michal-lisowski@outlook.com>
2015-11-05 19:33:19 +0100

    Update runAway.py

--- runAway.py
+++ runAway.py
@@ -9,33 +9,17 @@ import tty
 import termios
 import threading
 
-
-
-#read distance in thread
-running = True
-def read_distance():
-    global running
-    while running:
-        print pi2go.getDistance()
-        if pi2go.getDistance() <= 10:
-            pi2go.reverse(speed);
-            time.sleep(3)
-            pi2go.stop()
-        time.sleep(0.25)
-
-
-
-
-
 # main loop
 speed = 100
 pi2go.init()
-thread = threading.Thread(target = read_distance)
-thread.start()
-    
 
-finally:
-    running = False
-    time.sleep(0.25)
-    pi2go.cleanup()
+while true:
+    print pi2go.getDistance()
+    if pi2go.getDistance() <= 10:
+        pi2go.reverse(speed);
+        time.sleep(3)
+        pi2go.stop()
+    time.sleep(0.25)   
+
+pi2go.cleanup()
     
