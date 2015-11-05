34caa8748348a2d7e4b8347e8065a47e7b4dc496
lisekAX <michal-lisowski@outlook.com>
2015-11-05 22:20:43 +0100

    Update lineFollower.py

--- lineFollower.py
+++ lineFollower.py
@@ -18,7 +18,7 @@ try:
         if (pi2go.irLeftLine() == False):
             pi2go.go(speed * 0.94, speed)
         elif (pi2go.irLeftLine() == True):
-            pi2go.spinleft(speed)   
+            pi2go.spinLeft(speed)   
         #if (pi2go.irLeftLine() == True and pi2go.irRightLine() == True):
         #    pi2go.go(speed * 0.94, speed)
         #elif (pi2go.irLeftLine() == False):
