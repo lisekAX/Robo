537c94ed8d2108e7ef46c589fa6b3051d271e695
lisekAX <michal-lisowski@outlook.com>
2015-11-06 20:12:02 +0100

    Update lineFollower.py

--- lineFollower.py
+++ lineFollower.py
@@ -15,18 +15,14 @@ pi2go.init()
 
 try:
     while True:
-        if (pi2go.irLeftLine() == False):
+        if (pi2go.irLeftLine() == True and pi2go.irRightLine() == True):
             pi2go.go(speed * 0.94, speed)
-        elif (pi2go.irLeftLine() == True):
-            pi2go.spinLeft(speed)   
-        #if (pi2go.irLeftLine() == True and pi2go.irRightLine() == True):
-        #    pi2go.go(speed * 0.94, speed)
-        #elif (pi2go.irLeftLine() == False):
-        #    pi2go.spinLeft(speed)
-        #elif (pi2go.irRightLine() == False):
-        #    pi2go.spinRight(speed)
-        #elif (pi2go.irLeftLine() == False and pi2go.irRightLine() == False):
-        #    pi2go.stop()
+        elif (pi2go.irLeftLine() == False):
+            pi2go.spinLeft(speed)
+        elif (pi2go.irRightLine() == False):
+            pi2go.spinRight(speed)
+        elif (pi2go.irLeftLine() == False and pi2go.irRightLine() == False):
+            pi2go.stop()
         #print(pi2go.irLeftLine())
         #print(pi2go.irRightLine())
         #print('#######')
